556790bb6053fd5ee7d3eb85a451f3202a0ea1b4
A. Svensson <github@lmas.se>
2021-01-14 16:40:56 +0100

    Updates AccessLog mw, shows more info

--- middlewares/accesslog_test.go
+++ middlewares/accesslog_test.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"net/http/httptest"
 	"testing"
 
 	"github.com/lmas/web/internal/assert"
@@ -22,5 +23,22 @@ func TestAccessLog(t *testing.T) {
 	assert.StatusCode(t, resp, http.StatusOK)
 	if len(buf.String()) < 1 {
 		t.Errorf("didn't get a log line")
+	}
+}
+
+func BenchmarkAccessLog(b *testing.B) {
+	h := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(200) // Do nothing else
+	})
+	var buf bytes.Buffer
+	mw := AccessLog(log.New(&buf, "", 0))
+	wrapped := mw(h)
+	w := httptest.NewRecorder()
+	r, _ := http.NewRequest("GET", "/", nil)
+
+	b.ReportAllocs()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		wrapped.ServeHTTP(w, r)
 	}
 }
