e2c1639af0d4b6b4c6ee6cc540416ccc15dd8cb7
A. Svensson <github@lmas.se>
2021-01-16 13:26:12 +0100

    Refactors handler to mux and handlers

--- mux.go
+++ mux.go
@@ -11,7 +11,6 @@ import (
 	"sync"
 
 	"github.com/julienschmidt/httprouter"
-	"github.com/pkg/errors"
 )
 
 // Handler is a shorter convenience function signature for http handlers, instead of
@@ -24,35 +23,6 @@ type Middleware func(Handler) Handler
 // RegisterFunc is a function signature used when you want to register multiple handlers under a common URL path.
 // First string is method, second string is the URL and last field is the Handler you want to register.
 type RegisterFunc func(string, string, Handler)
-
-// NotFoundHandler is the default "404 not found" handler. It simply calls http.NotFound()
-func NotFoundHandler(c *Context) error {
-	http.NotFound(c.W, c.R)
-	return nil
-}
-
-// ErrorHandler is the default error handler. The error is never nil.
-// It will log the error and call http.Error(). Defaults to "500 internal server error", but if it's an ErrorHTTP it
-// will send a custom status code and error message (from ErrorHTTP).
-func ErrorHandler(c *Context, e error) {
-	switch err := errors.Cause(e).(type) {
-	case *ErrorHTTP:
-		c.M.logError("HTTP", err, err.stack)
-		http.Error(c.W, err.Error(), err.Status())
-	case *ErrorPanic:
-		c.M.logError("Panic", err, err.stack)
-		http.Error(c.W, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
-	default:
-		c.M.logError("Unknown", err)
-		http.Error(c.W, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
-	}
-}
-
-// PanicHandler is the default handler called whenever a panic was recovered inside a handler. It simply calls
-// HandleError() with a ErrorPanic.
-func PanicHandler(c *Context, ret interface{}) {
-	c.M.opt.HandleError(c, &ErrorPanic{newStack(6), ret})
-}
 
 // MuxOptions contains all the optional settings for a Mux.
 type MuxOptions struct {
